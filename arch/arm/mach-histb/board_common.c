ead138766e8832e218b55d71b7a06eaea90f6291
Yang Xiwen <forbidden405@foxmail.com>
2023-03-25 21:31:31 +0800

    arm: add support for Hisilicon HiSTB family SoCs
    First supported chip is hi3798mv200 (which is similar to Hi3798cv200
    used by poplar).
    
    Signed-off-by: Yang Xiwen <forbidden405@foxmail.com>

---
+++ arch/arm/mach-histb/board_common.c
@@ -0,0 +1,30 @@
+/*
+ * Board init file for all histb boards
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <fdtdec.h>
+#include <init.h>
+#include <asm/system.h>
+
+int __weak board_init(void)
+{
+	return 0;
+}
+
+int __weak dram_init_banksize(void)
+{
+	return fdtdec_setup_memory_banksize();
+}
+
+int __weak dram_init(void)
+{
+	return fdtdec_setup_mem_size_base();
+}
+
+void __weak reset_cpu(void)
+{
+	psci_system_reset();
+}
