a9ab474fc784959a6f8851eeda80f5cfad2f4729
Yang Xiwen <forbidden405@outlook.com>
2024-01-21 23:06:46 +0800

    board: hc2910-2aghd05: fix SDIO LDO configuration
    Somehow the first commit exchanged address and value, which makes the
    configuration invalid. Fix that by exchaning address and value back.
    
    Fixes: 33f19038cc6b ("arm: histb: hi3798mv200: add initial support for Hi3798MV200 HC2910-2AGHD05 board")
    Signed-off-by: Yang Xiwen <forbidden405@outlook.com>

--- board/skyworth/hc2910-2aghd05/hc2910-2aghd05.c
+++ board/skyworth/hc2910-2aghd05/hc2910-2aghd05.c
@@ -12,10 +12,12 @@
 #define HI3798MV200_PERI_CTRL_BASE 0xf8a20000
 #define SDIO0_LDO_OFFSET 0x11c
 
+#define SDIO0_LDO_VAL	0x60
+
 static int sdio0_set_ldo(void)
 {
 	// SDIO LDO bypassed, 3.3V
-	writel(HI3798MV200_PERI_CTRL_BASE + SDIO0_LDO_OFFSET, 0x60);
+	writel(SDIO0_LDO_VAL, HI3798MV200_PERI_CTRL_BASE + SDIO0_LDO_OFFSET);
 	return 0;
 }
 
