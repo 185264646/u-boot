0dfc49e152ec64d4c4dc1bf84ca5eda74c6e8c4d
Yang Xiwen <forbidden405@outlook.com>
2024-01-27 02:25:03 +0800

    arm: mach-histb: fix sysmap
    Actually peripheral blocks begin at 0xF0000000, DRAM ends at this
    address too.
    
    Signed-off-by: Yang Xiwen <forbidden405@outlook.com>

--- arch/arm/mach-histb/sysmap-histb.c
+++ arch/arm/mach-histb/sysmap-histb.c
@@ -12,13 +12,13 @@ static struct mm_region histb_mem_map[] = {
 	{
 		.virt = 0x0UL, /* DRAM */
 		.phys = 0x0UL,
-		.size = 0x80000000UL,
+		.size = 0xF0000000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
 			 PTE_BLOCK_INNER_SHARE
 	}, {
-		.virt = 0x80000000UL, /* Peripheral block */
-		.phys = 0x80000000UL,
-		.size = 0x80000000UL,
+		.virt = 0xF0000000UL, /* Peripheral block */
+		.phys = 0xF0000000UL,
+		.size = 0x10000000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
