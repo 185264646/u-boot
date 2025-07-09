3ddb837e53696c182bc98c53ee01ddd1effab262
Yang Xiwen <forbidden405@outlook.com>
2025-07-10 00:18:24 +0800

    dm: core: allow -ENODEV for power domain
    With CONFIG_POWER_DOMAIN enabled, dev_power_domain_on() returns -ENODEV
    when it fails to probe the pm supplier. It's common when using upstream
    dts while pm driver is missing in u-boot. And should not be treated as
    an error. Don't fail in this case.
    
    Signed-off-by: Yang Xiwen <forbidden405@outlook.com>

--- drivers/core/device.c
+++ drivers/core/device.c
@@ -522,7 +522,7 @@ int device_probe(struct udevice *dev)
 	    (device_get_uclass_id(dev) != UCLASS_POWER_DOMAIN) &&
 	    !(drv->flags & DM_FLAG_DEFAULT_PD_CTRL_OFF)) {
 		ret = dev_power_domain_on(dev);
-		if (ret)
+		if (ret && ret != -ENODEV)
 			goto fail;
 	}
 
